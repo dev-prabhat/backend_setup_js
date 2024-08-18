a8fb63985bd90b0b3d5dbfe2e8e657c11501d638
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-18 19:42:53 +0530

    initial db and express server connection

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Error", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on PORT`);
+    });
+  })
+  .catch((error) => {
+    console.log("EXPRESS server ERROR", error)
+    process.exit(1)
+  });
+console.log("Hello there");
