c828cc4f0973d204f4c6a5e26aeafa8e6ada3869
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-18 19:42:00 +0530

    created uitils functions

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+// API Success response
+class ApiResponse {
+    constructor(statusCode, data, message = 'Success'){
+     this.statusCode = statusCode
+     this.data = data
+     this.message = message
+     this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,8 @@
+// Wrapper function 
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
