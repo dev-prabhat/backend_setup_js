b69a1557238b2f584f0c88001f5fdd13905ce513
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-24 18:49:59 +0530

    refactor: added getALlProducts controller

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { createProduct } from '../controllers/product.controller.js'
+import { createProduct, getAllProducts } from '../controllers/product.controller.js'
 
 const router = Router()
 
-router.route('/p').get()
+router.route('/p').get(getAllProducts)
 router.route('/createProduct').post(createProduct)
 
 export default router
