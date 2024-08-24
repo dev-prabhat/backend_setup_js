a04f7debbdae0c2fe15da888252aceeea2f75213
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-24 18:48:36 +0530

    fix: categoryControllers

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -28,16 +28,28 @@ const getAllCategories = asyncHandler(async (req, res) => {
 
 // create different product categories
 const createCategory = asyncHandler(async (req, res) => {
-  const { name, gstRate } = req.body;
+  const { category, gstRate } = req.body;
 
-  if (!name || !gstRate) {
+  if (!category.trim() || !gstRate) {
     throw res
       .status(400)
-      .json(new ApiError(400, "", ["Name and gst  is missing"]));
+      .json(new ApiError(400, "", ["CategoryName and GSTRate are missing"]));
+  }
+
+  const categoryAlreadyExist = await Category.findOne({name: category.toLowerCase()})
+
+  if(categoryAlreadyExist){
+    throw res
+      .status(500)
+      .json(
+        new ApiError(500, "", [
+          "Category Already exist",
+        ])
+      );
   }
 
   const categoryCreated = await Category.create({
-    name,
+    name:category.toLowerCase(),
     gstRate,
   });
 
@@ -57,49 +69,5 @@ const createCategory = asyncHandler(async (req, res) => {
     );
 });
 
-// updating category details
-const updateCategory = asyncHandler(async (req, res) => {
-  const { categoryId } = req.params;
-  const { name, gstRate } = req.body;
-
-  if (!isValidObjectId(categoryId)) {
-    throw res
-      .status(400)
-      .json(new ApiError(400, "", ["category does not exist"]));
-  }
-
-  if (!name || !gstRate) {
-    throw res.status(400).json(new ApiError(400, "", ["name or gst is empty"]));
-  }
-
-  const category = await Category.findById(categoryId);
-
-  if (!category) {
-    throw res
-      .status(400)
-      .json(new ApiError(400, "", ["failed to fetched category from DB"]));
-  }
-
-  const updatedCategory = await Category.findByIdAndUpdate(
-    categoryId,
-    {
-      $set: {
-        name,
-        gstRate,
-      },
-    },
-    { new: true }
-  );
-
-  if(!updateCategory){
-    throw res.status(500).json(new ApiError(500,'',["Error while updating category details"]));
-  }
-
-  return res
-  .status(200)
-  .json(new ApiResponse(200, updatedCategory, "Category updated successfully"));
-});
-
-// const deleteCategory = asyncHandler(async (req,res) => {})
 
 export { createCategory, getAllCategories };
