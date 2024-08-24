4e13d344015bf99ccc3740e1255f18805d9e0e12
dev-prabhat <prabhatsingh55rocks@gmail.com>
2024-08-25 00:11:59 +0530

    added: auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw res
+        .status(401)
+        .json(new ApiError(401, "", ["Unauthorized request"]));
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password");
+
+    if (!user) {
+      throw res
+        .status(401)
+        .json(new ApiError(401, "", ["Invalid Access token"]));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw res.status(401).json(new ApiError(401, "", ["Invalid Access token"]));
+  }
+});
